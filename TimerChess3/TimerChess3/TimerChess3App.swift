d0376fd38345d309250ad514300a1b7e09d3ba87
Giovanni Di Fenza <giovannijr2000@gmail.com>
2025-03-15 19:54:47 +0100

    Add files via upload

---
+++ TimerChess3/TimerChess3/TimerChess3App.swift
@@ -0,0 +1,17 @@
+//
+//  TimerChess3App.swift
+//  TimerChess3
+//
+//  Created by Giovanni Jr Di Fenza on 15/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TimerChess3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
